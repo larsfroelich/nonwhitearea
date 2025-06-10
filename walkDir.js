bb1ff808f43a89574c8972020fe4dd8543f53d11
Lars Frölich <lars.froelich@gmail.com>
2025-06-10 15:04:42 +0200

    Use path.join in walkDir

--- walkDir.js
+++ walkDir.js
@@ -1,11 +1,12 @@
 const fs = require('fs');
+const path = require('path');
 global.walkDirectory = function(rootdir, subdir, depth) {
     var results = [];
-    var list = fs.readdirSync(rootdir + subdir);
+    var list = fs.readdirSync(path.join(rootdir, subdir));
     list.forEach(function(file) {
-        var stat = fs.statSync(rootdir + subdir + '/' + file);
-        if (stat && stat.isDirectory() && depth > 1) results = results.concat(walkDirectory(rootdir, subdir + '/' + file, depth-1));
-        else results.push(subdir + '/' + file)
+        var stat = fs.statSync(path.join(rootdir, subdir, file));
+        if (stat && stat.isDirectory() && depth > 1) results = results.concat(walkDirectory(rootdir, path.join(subdir, file), depth-1));
+        else results.push(path.join(subdir, file))
     });
     return results
 };
